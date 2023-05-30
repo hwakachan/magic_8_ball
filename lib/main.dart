76744276036364e261a03b8c04aa74d2db88d41e
hwakachan <hi_wakachan0823@icloud.com>
2023-05-30 22:58:15 +0900

    create a new Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,31 @@ void main() => runApp(
       ),
     );
 
+class Ball extends StatefulWidget {
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Image.asset('images/ball1.png'),
+      ),
+    );
+  }
+}
+
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-        title: Text('Ask Me Anything'),
-        backgroundColor: Colors.blue.shade900,
-      ),
-      body: Container(),
-    );
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: Ball());
   }
 }
